96055c3bb04870630b88af4106fa57e037cc3d6e
Anony <ljx1583861@qq.com>
2022-08-14 16:09:23 +0800

    初始化

---
+++ src/views/dashboard/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="dashboard-container">
+    <div class="dashboard-text">name: {{ name }}</div>
+    <div class="block">
+      <span class="demonstration">页数较少时的效果</span>
+      <el-pagination
+        layout="prev, pager, next"
+        :total="50"
+      />
+    </div>
+    <div class="block">
+      <span class="demonstration">大于 7 页时的效果</span>
+      <el-pagination
+        layout="prev, pager, next"
+        :total="1000"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'Dashboard',
+  computed: {
+    ...mapGetters([
+      'name'
+    ])
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.dashboard {
+  &-container {
+    margin: 30px;
+  }
+  &-text {
+    font-size: 30px;
+    line-height: 46px;
+  }
+}
+</style>
