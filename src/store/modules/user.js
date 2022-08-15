272f13e45e01b659bbfa4af552a6474a07d54d3b
yang <young_yang95@163.com>
2022-08-15 16:02:03 +0800

    登录token存储，跳转

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,6 +1,27 @@
+import { login } from '@/api/user'
+import { getToken, setToken, removeToken } from '@/utils/auth'
+
 export default {
   namespaced: true,
-  state: {},
-  mutations: {},
-  actions: {}
+  state: {
+    token: getToken()
+  },
+  mutations: {
+    setToken(state, token) {
+      state.token = token
+      setToken(token)
+    },
+    removeToken(state) {
+      state.token = null
+      removeToken()
+    }
+  },
+  actions: {
+    // 通过接口获取token
+    async login({ commit }, data) {
+      const res = await login(data)
+      console.log(res)
+      commit('setToken', res.data.data)
+    }
+  }
 }
