03445cab8120663bded79d34bb002b9aa6648c09
yang <young_yang95@163.com>
2022-08-22 19:32:55 +0800

    公司

---
+++ src/views/departments/components/tree-tools.vue
@@ -0,0 +1,70 @@
+<template>
+  <el-row
+    type="flex"
+    justify="space-between"
+    align="middle"
+    style="width: 100%"
+  >
+    <!-- 左侧内容 -->
+    <el-col>
+      <span>{{ treeNode.name }}</span>
+    </el-col>
+    <!-- 右侧内容 -->
+    <el-col :span="4">
+      <el-row type="flex" justify="end">
+        <!-- 负责人 -->
+        <el-col>{{ treeNode.manager }}</el-col>
+        <!-- 操作 -->
+        <el-col>
+          <!-- 放置下拉菜单 -->
+          <el-dropdown @command="handleCommand">
+            <!-- 内容 -->
+            <span>操作
+              <i class="el-icon-arrow-down" />
+            </span>
+            <!-- 具名插槽 -->
+            <el-dropdown-menu slot="dropdown">
+              <!-- 下拉选项 -->
+              <el-dropdown-item
+                command="add"
+              >添加子部门</el-dropdown-item>
+              <el-dropdown-item v-if="isRoot">编辑部门</el-dropdown-item>
+              <el-dropdown-item v-if="isRoot">删除部门</el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
+        </el-col>
+      </el-row>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+export default {
+  props: {
+    //   定义一个props属性
+    treeNode: {
+      type: Object, // 对象类型
+      required: true // 要求对方使用您的组件的时候 必须传treeNode属性 如果不传 就会报错
+    },
+    isRoot: {
+      // isRoot 来控制 编辑部门 和 删除部门 的 显示 隐藏
+      type: Boolean,
+      default: true // 默认显示
+    }
+  },
+  methods: {
+    handleCommand(type) {
+      if (type === 'add') {
+        // 新增
+        this.$emit('addDepts', this.treeNode)
+      } else if (type === 'edit') {
+        // 编辑
+      } else {
+        // 删除
+      }
+    }
+  }
+}
+</script>
+
+<style></style>
--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -1,19 +1,72 @@
 <template>
-  <div>组织架构</div>
+  <div class="dashboard-container">
+    <div class="app-container">
+      <!-- 实现页面的基本布局 -->
+      <el-card class="tree-card">
+        <!-- 用了一个行列布局 -->
+        <!-- 缺少treeNode -->
+        <tree-tools :tree-node="company" class="title" />
+        <!--放置一个属性   这里的props和我们之前学习的父传子 的props没关系-->
+        <el-tree :data="departs" :props="defaultProps" default-expand-all>
+          <!-- 说明el-tree里面的这个内容 就是插槽内容 => 填坑内容  => 有多少个节点循环多少次 -->
+          <!-- scope-scope 是 tree组件传给每个节点的插槽的内容的数据 -->
+          <!-- 顺序一定是 执行slot-scope的赋值 才去执行 props的传值 -->
+          <tree-tools
+            slot-scope="obj"
+            :tree-node="obj.data"
+            @delDepts="getDepartments"
+            @addDepts="addDepts"
+          />
+        </el-tree>
+        <AddDept :dialog-visible="dialogVisible" />
+      </el-card>
+    </div>
+  </div>
 </template>
 
 <script>
+import TreeTools from './components/tree-tools.vue'
+import { getDepartments } from '@/api/departments'
+import { tranListToTreeData } from '@/utils/index'
+import AddDept from './components/add-dept' // 引入新增部门组件
 export default {
-  name: 'Hrsaas1Index',
-
+  name: 'Departments',
+  components: { TreeTools, AddDept },
   data() {
-    return {}
+    return {
+      company: {}, // 就是头部的数据结构
+      departs: [],
+      defaultProps: {
+        label: 'name'
+      },
+      dialogVisible: false // 显示窗体
+    }
   },
-
-  mounted() {},
-
-  methods: {}
+  created() {
+    this.getDepartments() // 调用自身的方法
+  },
+  methods: {
+    async getDepartments() {
+      const result = await getDepartments()
+      this.company = { name: result.companyName, manager: result.companyManage }
+      this.departs = tranListToTreeData(result.depts, '') // 需要将其转化成树形结构
+      console.log(result)
+    },
+    addDepts(node) {
+      this.dialogVisible = true // 显示弹层
+      // 因为node是当前的点击的部门， 此时这个部门应该记录下来,
+      this.node = node
+    }
+  }
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.departments-container {
+  width: 900px;
+  margin: 20px auto;
+}
+.title {
+  margin-bottom: 15px;
+}
+</style>
