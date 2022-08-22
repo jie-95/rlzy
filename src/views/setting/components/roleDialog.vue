b42bf7cc23450c55b5c75f9325e3854f095b0673
yang <young_yang95@163.com>
2022-08-22 14:29:25 +0800

    操作-删除、编辑完成

--- src/views/setting/components/roleDialog.vue
+++ src/views/setting/components/roleDialog.vue
@@ -1,15 +1,24 @@
 <template>
   <el-dialog
-    title="新增角色"
+    :title="`${roleForm.id ? '修改' : '新增'}角色`"
     :visible.sync="dialogVisible"
     :before-close="handleClose"
   >
-    <el-form ref="roleDialogForm" label-width="80px">
-      <el-form-item prop="name" label="角色">
-        <el-input />
+    <el-form ref="roleDialogForm" label-width="80px" :model="roleForm">
+      <el-form-item
+        prop="name"
+        label="角色"
+        :rules="[{ required: true, message: '角色必填', trigger: 'blur' }]"
+      >
+        <el-input v-model="roleForm.name" placeholder="请输入角色" />
       </el-form-item>
       <el-form-item label="描述">
-        <el-input type="textarea" row="3" />
+        <el-input
+          v-model="roleForm.description"
+          type="textarea"
+          row="3"
+          placeholder="请输入描述"
+        />
       </el-form-item>
     </el-form>
     <el-row
@@ -19,31 +28,69 @@
       align="middle"
       class="dialog-footer"
     >
-      <el-button>取 消</el-button>
-      <el-button type="primary">确 定</el-button>
+      <el-button @click="handleClose">取 消</el-button>
+      <el-button
+        type="primary"
+        :loading="loading"
+        @click="submitRole"
+      >确 定</el-button>
     </el-row>
   </el-dialog>
 </template>
 
 <script>
+import { addRole, updateRole } from '@/api/settings'
 export default {
   name: 'Hrsaas1RoleDialog',
   props: {
     dialogVisible: {
-      type: Boolean
+      type: Boolean,
+      default: false
     }
   },
 
   data() {
     return {
       // dialogVisible: true
+      roleForm: {
+        name: '',
+        description: ''
+      },
+      loading: false
     }
   },
   methods: {
     handleClose() {
-      console.log(111)
-      this.dialogVisible = false
-      this.$parent.dialogVisible = false
+      this.$emit('update:dialogVisible', false)
+      this.$refs.roleDialogForm.resetFields()
+
+      // 清除修改角色是传进来的id
+      this.roleForm = {
+        name: '',
+        description: ''
+      }
+      // 清除修改角色是传进来的id
+    },
+    async submitRole() {
+      try {
+        // 表单验证
+        await this.$refs.roleDialogForm.validate()
+        this.loading = true
+        this.roleForm.id
+          ? await updateRole(this.roleForm)
+          : await addRole(this.roleForm)
+        // 调用父组件，更新列表
+        // this.$parent.getRoleList()
+        this.$emit('refresh')
+        // 提示更新成功
+        this.$message.success(`角色${this.roleForm.id ? '修改' : '新增'}成功`)
+        // 关闭弹窗
+        this.handleClose()
+      } catch (error) {
+        console.log(error)
+      } finally {
+        this.loading = false
+      }
     }
   }
 }
--- src/views/setting/index.vue
+++ src/views/setting/index.vue
@@ -1,48 +1,79 @@
 <template>
-  <el-tabs class="app-container">
-    <el-tab-pane label="角色管理" name="first">
-      <!-- 新增角色按钮 -->
-      <el-row style="height: 60px">
-        <el-button
-          icon="el-icon-plus"
-          size="small"
-          type="primary"
-          @click="dialogVisible = true"
-        >新增角色</el-button>
-      </el-row>
-      <!-- 表格 -->
-      <el-table v-loading="loading" border :data="list" width="100%">
-        <el-table-column label="序号" width="80" type="index" />
-        <el-table-column prop="name" label="角色名称" width="240" />
-        <el-table-column prop="description" label="描述" />
-        <el-table-column label="操作" width="320">
-          <el-button size="small" type="success">分配权限</el-button>
-          <el-button size="small" type="primary">编辑</el-button>
-          <el-button size="small" type="danger">删除</el-button>
-        </el-table-column>
-      </el-table>
-      <!-- 分页 -->
-      <el-row type="flex" justify="end" style="height: 60px" align="middle">
-        <el-pagination
-          background
-          :current-page.sync="page.page"
-          layout="total,prev, pager, next, sizes"
-          :page-size.sync="page.pagesize"
-          :page-sizes="[5, 10, 15, 20]"
-          :total="total"
-          @current-change="changePage"
-          @size-change="changeSize"
-        />
-      </el-row>
-    </el-tab-pane>
-    <el-tab-pane label="公司信息" name="second">公司信息</el-tab-pane>
-    <RoleDialog :dialog-visible="dialogVisible" />
-  </el-tabs>
+  <div>
+    <el-tabs class="app-container">
+      <el-tab-pane label="角色管理" name="first">
+        <!-- 新增角色按钮 -->
+        <el-row style="height: 60px">
+          <el-button
+            icon="el-icon-plus"
+            size="small"
+            type="primary"
+            @click="add"
+          >新增角色</el-button>
+        </el-row>
+        <!-- 表格 -->
+        <el-table v-loading="loading" border :data="list" width="100%">
+          <el-table-column label="序号" width="80" type="index" />
+          <el-table-column prop="name" label="角色名称" width="240" />
+          <el-table-column prop="description" label="描述" />
+          <el-table-column label="操作" width="320">
+            <template slot-scope="scope">
+              <el-button size="small" type="success">分配权限</el-button>
+              <el-button
+                size="small"
+                type="primary"
+                @click="edit(scope.row)"
+              >编辑</el-button>
+              <el-button
+                size="small"
+                type="danger"
+                @click="del(scope.row.id)"
+              >删除</el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+        <!-- 分页 -->
+        <el-row type="flex" justify="end" style="height: 60px" align="middle">
+          <el-pagination
+            background
+            :current-page.sync="page.page"
+            layout="total,prev, pager, next, sizes"
+            :page-size.sync="page.pagesize"
+            :page-sizes="[5, 10, 15, 20]"
+            :total="total"
+            @current-change="changePage"
+            @size-change="changeSize"
+          />
+        </el-row>
+      </el-tab-pane>
+      <el-tab-pane label="公司信息" name="second">
+        <el-form label-width="120px" style="margin-top: 50px">
+          <el-form-item label="公司名称">
+            <el-input disabled style="width: 400px" />
+          </el-form-item>
+          <el-form-item label="公司地址">
+            <el-input disabled style="width: 400px" />
+          </el-form-item>
+          <el-form-item label="邮箱">
+            <el-input disabled style="width: 400px" />
+          </el-form-item>
+          <el-form-item label="备注">
+            <el-input type="textarea" :rows="3" disabled style="width: 400px" />
+          </el-form-item>
+        </el-form>
+      </el-tab-pane>
+    </el-tabs>
+    <RoleDialog
+      ref="roleDialog"
+      :dialog-visible.sync="dialogVisible"
+      @refresh="getRoleList"
+    />
+  </div>
 </template>
 
 <script>
 import RoleDialog from './components/roleDialog.vue'
-import { getRoleList } from '@/api/settings'
+import { getRoleList, deleteRole } from '@/api/settings'
 export default {
   name: 'Hrsaas1Index',
   components: {
@@ -51,6 +82,7 @@ export default {
 
   data() {
     return {
+      activeName: 'second',
       page: {
         page: 1,
         pagesize: 10
@@ -86,6 +118,37 @@ export default {
     },
     changeSize() {
       this.getRoleList()
+    },
+    // 新增角色
+    add() {
+      this.dialogVisible = true
+    },
+    edit(row) {
+      console.log(row)
+      this.dialogVisible = true
+      this.$refs.roleDialog.roleForm = { ...row }
+    },
+    async del(id) {
+      console.log(id)
+      // this.$alert('这是一段内容', '标题名称', {
+      //   confirmButtonText: '确定',
+      //   callback: (action) => {
+      //     this.$message({
+      //       type: 'info',
+      //       message: `action: ${action}`
+      //     })
+      //   }
+      // })
+      try {
+        await this.$confirm('是否确认删除该角色？', '提示', { type: 'warming' })
+        await deleteRole(id)
+        this.$message.success('角色删除成功')
+        this.page.page = 1
+        console.log('=========')
+        this.getRoleList()
+      } catch (error) {
+        console.log(error)
+      }
     }
   }
 }
